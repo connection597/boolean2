73dc4eb1a945adad25bbece30e0fdfeef984f68f
connection579 <wlswn4118@naver.com>
2020-10-29 13:39:20 +0900

    부울의 조건문

---
+++ src/boolean2/BooleanEx1.java
@@ -0,0 +1,16 @@
+package boolean2;
+
+public class BooleanEx1 {
+
+	public static void main(String[] args) { //부울 존건문
+		int base = 100;
+		int height =185;
+		boolean isTall = height >  base;
+		
+		if(isTall) {
+			System.out.println("키가 큽니다");
+		}
+
+	}
+
+}
